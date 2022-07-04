3140ffc1cfd5847ec0f1122b6c0ad701bd6bd84a
Yashaswini.M <yashaswini.m@thundersoft.com>
2022-07-04 17:51:31 +0530

    Adding programs on variables

---
+++ variables/float.py
@@ -0,0 +1,10 @@
+a=10.2
+b=20.2
+print(a,b)
+
+a,b,c,d=10.2,20.5,2.67,34.5
+print(a,b,c,d)
+print(d,c,b,a)
+
+
+
---
+++ variables/integer.py
@@ -0,0 +1,20 @@
+a=10
+print(a)
+
+a=10
+b=20
+c=a+b
+print(a+b)
+
+a1=input('enter first number:')
+b2=input('enter second number:')
+sum=float(a1)+float(b2)
+print('the sum of{0} and{1} is{2}'.format(a1,b2,sum))
+
+a=b=c=5
+print(a,b,c)
+
+a,b,c,d=10,20.5,"python",'true'
+print(a,b,c,d)
+print(d,c,b,a)
+
