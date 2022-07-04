2540c96472708c09ffbad04ae06f78b0ea3cf1bd
Yashaswini.M <yashaswini.m@thundersoft.com>
2022-07-04 17:54:02 +0530

    Adding program on control structure

---
+++ control_structure/control_structure.py
@@ -0,0 +1,49 @@
+#control structure
+#Sequence
+day=input("Enter a day")
+print("today is"+day)
+print("Today is ",day)
+
+#selection
+x=5
+if x > 0:
+    print("x is +ve")
+
+
+
+if x>0:
+    print("x is +ve")
+else:
+    print("x is not +ve")
+         
+x=int(input())
+if x >0:
+    print("x is +ve")
+elif x<0:
+    print("x is -ve")
+else:
+    print("x is 0")
+            
+#iteration(loops)
+for x in["c","c++","python"]:
+    print(x)
+a=10
+while a>0:
+    print("a is:",a)
+    a=a-2
+for a in [1,2]:
+ for b in [1,2]:
+    print(a,b)
+
+        
+#Range()
+for a in range(3):
+    for b in range(3):
+        print(a,b)
+
+num = 7
+for i in range(1,num,2):
+    print(num)
+
+
+
